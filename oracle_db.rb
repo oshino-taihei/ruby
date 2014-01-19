d3a66ae72efb4d2e172abe093f7f8820489f86be
Taihei Oshino <oshino.taihei+git@gmail.com>
2014-01-19 16:58:49 +0900

    add oracel connector

---
+++ oracle_db.rb
@@ -0,0 +1,22 @@
+# coding: UTF-8
+require 'oci8'
+
+user='JPSPFA11'
+pass='JPSPFA11#'
+tns='JDC11'
+sql="select * from tsy_inf"
+
+begin
+  conn = OCI8.new(user, pass, tns)
+  cursor = conn.exec(sql)
+  puts cursor.get_col_names.join(',')
+  #puts cursor.column_metadata
+  while row = cursor.fetch
+    puts row.join(',')
+  end
+  puts "#{cursor.row_count} rows fetched."
+
+  cursor.close
+  ensure
+  conn.logoff if conn
+end
