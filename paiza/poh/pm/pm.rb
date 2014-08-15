cbbe26894997d5f533866a5785babcc5870a508f
Taihei Oshino <oshino.taihei+git@gmail.com>
2014-08-16 08:28:10 +0900

    正答を出力するよう修正。ただし大規模データはタイムアウト。

--- paiza/poh/pm/pm.rb
+++ paiza/poh/pm/pm.rb
@@ -7,7 +7,9 @@ $n.times {
   $offices.push(office)
 }
 
-$min_cost = -1
+# 最小コスト (全コストのトータルで初期化)
+$min_cost = $offices.inject(0) { |sum,o| sum + o[1] }
+
 # arg: i(i番目までのoffice)
 # return: [人数トータル, コストトータル]
 def solve(i, num_total, cost_total)
@@ -19,16 +21,14 @@ def solve(i, num_total, cost_total)
     next_cost_total = cost_total + $offices[i][1]
     if next_num_total >= $m then
       $min_cost = [$min_cost, next_cost_total].min
-      $min_cost = next_cost_total if $min_cost == -1
     end
     solve(i+1, next_num_total, next_cost_total)
 
     # i番目を加えない場合
-    next_num_total = $offices[i][0]
-    next_cost_total = $offices[i][1]
+    next_num_total = num_total
+    next_cost_total = cost_total
     if next_num_total >= $m then
       $min_cost = [$min_cost, next_cost_total].min
-      $min_cost = next_cost_total if $min_cost == -1
     end
     solve(i+1, next_num_total, next_cost_total)
   end
