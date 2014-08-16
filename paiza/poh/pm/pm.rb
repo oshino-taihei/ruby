1cdd71fc090242a6af15c68b6a11598f7f9316b6
Taihei Oshino <oshino.taihei+git@gmail.com>
2014-08-16 17:21:57 +0900

    不要なswapファイルを削除

--- paiza/poh/pm/pm.rb
+++ paiza/poh/pm/pm.rb
@@ -10,27 +10,26 @@ $n.times {
 # 最小コスト (全コストのトータルで初期化)
 $min_cost = $offices.inject(0) { |sum,o| sum + o[1] }
 
-# arg: i(i番目までのoffice)
-# return: [人数トータル, コストトータル]
+################################
+#  i:i番目までのoffice
+#  num_total:人数のトータル
+#  cost_total: コストのトータル
+################################
 def solve(i, num_total, cost_total)
+  # プロジェクト最小人数を満たしているなら、最小コストを更新
+  $min_cost = [$min_cost, cost_total].min if num_total >= $m
+
+  # 全てのofficeを探索したなら終了
   if i == $n then
     return true
   else
     # i番目を加える場合
     next_num_total = num_total + $offices[i][0]
     next_cost_total = cost_total + $offices[i][1]
-    if next_num_total >= $m then
-      $min_cost = [$min_cost, next_cost_total].min
-    end
     solve(i+1, next_num_total, next_cost_total)
 
     # i番目を加えない場合
-    next_num_total = num_total
-    next_cost_total = cost_total
-    if next_num_total >= $m then
-      $min_cost = [$min_cost, next_cost_total].min
-    end
-    solve(i+1, next_num_total, next_cost_total)
+    solve(i+1, num_total, cost_total)
   end
   return true
 end
